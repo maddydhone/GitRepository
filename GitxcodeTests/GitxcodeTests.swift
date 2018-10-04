0b0154a528d8d934a9e4ee64a6b329a7614f1501
Admin <admin@Admins-Mac.local>
2018-10-04 18:42:25 +0530

    Initial Commit

---
+++ GitxcodeTests/GitxcodeTests.swift
@@ -0,0 +1,36 @@
+//
+//  GitxcodeTests.swift
+//  GitxcodeTests
+//
+//  Created by Admin on 04/10/2018.
+//  Copyright © 2018 Avansys. All rights reserved.
+//
+
+import XCTest
+@testable import Gitxcode
+
+class GitxcodeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
